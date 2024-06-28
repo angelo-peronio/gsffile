4c87d9e1dc30f33b48d833dc13cafab000468093
Angelo Peronio <22264609+angelo-peronio@users.noreply.github.com>
2024-06-28 15:17:22 +0200

    Ruff

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Read and write Gwyddion Simple Field files
 
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+
 `gsf_file` is Python library to read and write [Gwyddion Simple Field](http://gwyddion.net/documentation/user-guide-en/gsf.html) (.gsf) files.
+
+## Roadmap
+
+- [x] Package
+- [x] Test
+- [x] GitHub
+- [x] Ruff
+- [ ] Modernize code
+- [ ] PyPI
+- [ ] CI
+- [ ] Best preactices <https://learn.scientific-python.org/development/guides/packaging-simple/>
+- [ ] Auto-versioning
+- [ ] Conda recipe
--- src/gsf_file/write.py
+++ src/gsf_file/write.py
@@ -1,7 +1,7 @@
 def write_gsf(data, file_name, metadata={}):
-    '''Write a 2D array to a Gwyddion Simple Field 1.0 file format
+    """Write a 2D array to a Gwyddion Simple Field 1.0 file format
     http://gwyddion.net/documentation/user-guide-en/gsf.html
-    
+
     Args:
         file_name (string): the name of the output (any extension will be replaced)
         data (2darray): an arbitrary sized 2D array of arbitrary numeric type
@@ -9,33 +9,32 @@ def write_gsf(data, file_name, metadata={}):
 
     Returns:
         nothing
-    '''
+    """
 
     XRes = data.shape[0]
     YRes = data.shape[1]
 
-    data = data.astype('float32')
-    
-    if file_name.rpartition('.')[1] == '.':
-        file_name = file_name[0:file_name.rfind('.')]
-    
-    gsfFile = open(file_name + '.gsf', 'wb')
+    data = data.astype("float32")
 
-    s = ''
-    s += 'Gwyddion Simple Field 1.0' + '\n'
-    s += 'XRes = {0:d}'.format(XRes) + '\n'
-    s += 'YRes = {0:d}'.format(YRes) + '\n'
+    if file_name.rpartition(".")[1] == ".":
+        file_name = file_name[0 : file_name.rfind(".")]
+
+    gsfFile = open(file_name + ".gsf", "wb")
+
+    s = ""
+    s += "Gwyddion Simple Field 1.0" + "\n"
+    s += "XRes = {0:d}".format(XRes) + "\n"
+    s += "YRes = {0:d}".format(YRes) + "\n"
 
     for i in metadata.keys():
         try:
-            s += i + ' = ' + '{0:G}'.format(metadata[i]) + '\n'
-        except:
-            s += i + ' = ' + str(metadata[i]) + '\n'
-            
+            s += i + " = " + "{0:G}".format(metadata[i]) + "\n"
+        except ValueError:
+            s += i + " = " + str(metadata[i]) + "\n"
 
-    gsfFile.write(bytes(s, 'UTF-8'))
+    gsfFile.write(bytes(s, "UTF-8"))
 
-    gsfFile.write(b'\x00' * (4 - len(s) % 4))
+    gsfFile.write(b"\x00" * (4 - len(s) % 4))
 
     gsfFile.write(data.tobytes(None))
 
