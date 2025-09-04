64859c05aec7d276086fd0de5a03909d16fa95d9
Angelo Peronio <22264609+angelo-peronio@users.noreply.github.com>
2025-09-04 22:12:07 +0200

    Use uv sync in New-PythonVenv

--- scripts/utils/New-PythonVenv.ps1
+++ scripts/utils/New-PythonVenv.ps1
@@ -1,17 +1,18 @@
 #Requires -Version 7.4
 <#
     .SYNOPSIS
-    Create a Python virtual enviroment.
+    Create or refresh a Python virtual enviroment.
 
     .DESCRIPTION
     Usage:
         * copy into your project,
         * set the default values of the parameters at your convenience,
         * execute.
-    If a `pyproject.toml` is found, also installs the corresponding package
-    in editable mode, including its "dev" optional dependencies.
-    If a `requirements.txt` is found, also installs the packages listed therein.
+    This script requires a `pyproject.toml`, and
+        * if present, installs the corresponding package in editable mode
+        * if present, installs the "dev" dependency group,
     The virtual enviroment is replaced if it already exists.
+    The pinned versions specified e.g. in `uv.lock` or `pylock.toml` are ignored.
     Placing the environment outside the project folder avoids synchronization issues
     with Microsoft OneDrive.
     Requires uv <https://docs.astral.sh/uv/>.
@@ -25,7 +26,7 @@
 
 param (
     # Place the environemnt environemnt outside the project folder, in $VenvsRootFolder
-    [switch]$OutsideProjectFolder = $true,
+    [switch]$OutsideProjectFolder = $false,
     # Folder containing the enviroments created with -OutsideProjectFolder
     [string]$VenvsRootFolder = "C:\venvs",
     # Python version to use. If feasible, specify it in pyproject.toml or .python-version, instead.
@@ -42,20 +43,13 @@ $ErrorActionPreference = "Stop"
 $ProjectRoot = Join-Path $PSScriptRoot $ProjectRoot | Resolve-Path
 "Project root folder: $ProjectRoot" | Write-Host
 $ProjectName = Split-Path $ProjectRoot -Leaf
-$VenvFolder = $OutsideProjectFolder ? (Join-Path $VenvsRootFolder $ProjectName) : ".venv"
+if ($OutsideProjectFolder) {
+    $Env:UV_PROJECT_ENVIRONMENT = Join-Path $VenvsRootFolder $ProjectName
+}
 
 Push-Location $ProjectRoot
 try {
-    uv venv --python=$PythonVersion --prompt=$ProjectName --clear $VenvFolder
-
-    if (Test-Path -Path pyproject.toml -PathType Leaf) {
-        "Found pyproject.toml. Installing..." | Write-Host
-        uv pip install --python=$VenvFolder --group=dev --editable=.
-    }
-    if (Test-Path -Path requirements.txt -PathType Leaf) {
-        "Found requirements.txt. Installing..." | Write-Host
-        uv pip install --python=$VenvFolder --requirement requirements.txt
-    }
+    uv sync --upgrade
 }
 finally {
     Pop-Location
