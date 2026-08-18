39bc58b4ebc8bb7ec80b68e52fab42156aa35243
Angelo Peronio <22264609+angelo-peronio@users.noreply.github.com>
2026-08-18 18:08:55 +0200

    Skip cffconvert in pre-commit autoupdate

--- scripts/Update-PreCommitHooks.ps1
+++ scripts/Update-PreCommitHooks.ps1
@@ -8,4 +8,7 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Import-Module -Name "$PSScriptRoot\Utils.psm1"
 
-uv run $(Get-UvRunOptions) prek autoupdate --jobs=4
+# As of 2026-03-06, the latest release tag of cffconvert is 2.0.0.
+# It does not include the pre-commit hook, causing `pre-commit autoupdate` to fail.
+# https://github.com/citation-file-format/cffconvert/issues/389#issuecomment-2480672136
+uv run $(Get-UvRunOptions) prek autoupdate --exclude-repo="https://github.com/citation-file-format/cffconvert" --jobs=4
